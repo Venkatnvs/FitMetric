ac48cf394377c86ac766459c318a68eb29ccee52
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2024-01-10 19:14:57 +0530

    g

--- build_vercel.sh
+++ build_vercel.sh
@@ -5,6 +5,6 @@ python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input --clear
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python3.9 manage.py createsuperuser --noinput --email
+python3.9 manage.py createsuperuser --noinput --email=venkatnvs2005@gmail.com
 
 echo "End Build"
