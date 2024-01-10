c8cbafb65fb2895a6d0561887afb629767ffd81c
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2024-01-10 18:20:31 +0530

    added build for vercel

---
+++ build_vercel.sh
@@ -0,0 +1,10 @@
+set -o errexit
+echo "Build Start"
+
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --no-input --clear
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py createsuperuser --noinput
+
+echo "End Build"
