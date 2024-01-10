86ce073848cfd2abbe64df63c2a3d0ef4d133464
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2024-01-10 18:55:36 +0530

    updated settings.py

--- FitMetric/settings.py
+++ FitMetric/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from decouple import config
 from django.contrib import messages
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,10 +96,10 @@ WSGI_APPLICATION = 'FitMetric.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('POSTGRESQL_URL'),
+        conn_max_age=600
+    )
 }
 
 
