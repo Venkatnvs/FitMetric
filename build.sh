e84c3d6685fb3e2a6c6d74e8017d6826fc8f9369
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2024-01-11 11:15:30 +0530

    up build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput
+# python manage.py createsuperuser --noinput
